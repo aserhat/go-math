b60e51aff1593537f9bd6289b98258d95ecd189c
Allen Serhat <serhata@gmail.com>
2020-11-11 14:31:59 -0500

    Release 1.4.0

--- math.go
+++ math.go
@@ -1,20 +1,28 @@
+// Package math provides basic math functions.
 package math
 
+// Inputs setups the two numbers you want do math on
 type Inputs struct {
 	InputA int
 	InputB int
 }
 
+// Add adds InputA and InputB
 func Add(i Inputs) int {
 	return i.InputA + i.InputB
 }
 
+// Subtract subtracts InputB from InputA
 func Subtract(i Inputs) int {
 	return i.InputA - i.InputB
 }
+
+// Multiply multiplies InputA and InputB
 func Multiply(i Inputs) int {
 	return i.InputA * i.InputB
 }
+
+// Divide divides InputA and InputB
 func Divide(i Inputs) int {
 	return i.InputA / i.InputB
 }
