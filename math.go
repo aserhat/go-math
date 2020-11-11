c8517304d8298af7d8bde9af1df309636a1c1dc5
Allen Serhat <serhata@gmail.com>
2020-11-11 13:48:26 -0500

    Release 1.0.0

---
+++ math.go
@@ -0,0 +1,14 @@
+package math
+
+type Inputs struct {
+	InputA int
+	InputB int
+}
+
+func Add(i Inputs) int {
+	return i.InputA + i.InputB
+}
+
+func Subtract(i Inputs) int {
+	return i.InputA - i.InputB
+}
